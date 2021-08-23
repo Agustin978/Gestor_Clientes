488710fe8b738e9fafffa99d6811f8caf2e9decf
Agustin978 <loboagus10@gmail.com>
2021-08-23 01:41:40 -0300

    Main del programa

---
+++ Gestor/core.py
@@ -0,0 +1,9 @@
+"""Fichero principal del programa"""
+
+import menu
+
+def main():
+    menu.loop()
+
+if __name__=="__main__":  #Lo coloco en un if para comprobar que se ejecute solo cuando llame a core.py
+    main()
